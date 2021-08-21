a23fccf0931d66e848b01c00e981e355fb681b16
asymmentric <74967082+Asymmentric@users.noreply.github.com>
2021-08-21 14:30:26 +0000

    as

--- Program-3.c
+++ Program-3.c
@@ -1,3 +1,15 @@
+// Fill in the missing code in the below sample code which concatenates two given strings and store the result in another string.
+//Instructions:-
+// 1)Read two strings from the standard input device and write a loop to copy each character of the first string into third string till the end of the first string.
+// 2)Write another loop to copy each character of the second string into third string till the end of second string.
+// 3)Now place '\0' at the end of the third string.
+// 4)Finally, display the third string.
+
+//Expected input & output:-
+//Enter the first string: Nithin
+//Enter the second string:  kumar
+//The concatenated  string = Nithinkumar
+
 #include <stdio.h>
 #include<string.h>
 int main() {
